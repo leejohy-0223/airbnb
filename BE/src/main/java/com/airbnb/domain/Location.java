61e07c567e53b173e677c472a4cbf016dd836149
jiwoo <zbqmgldjfh@gmail.com>
2022-05-26 10:59:55 +0900

    [feat]: 좌표 저장용 Location 클래스 구현

---
+++ BE/src/main/java/com/airbnb/domain/Location.java
@@ -0,0 +1,23 @@
+package com.airbnb.domain;
+
+public class Location {
+
+    private Double latitude;
+    private Double longitude;
+
+    public Location() {
+    }
+
+    public Location(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+}
