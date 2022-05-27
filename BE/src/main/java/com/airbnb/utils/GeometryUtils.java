811bc5ef756b28cc7e1636616c2f5456090ba108
jiwoo <zbqmgldjfh@gmail.com>
2022-05-27 10:44:04 +0900

    [feat]: GeometryUtils.toPoint 구현
    - 위도, 경도를 받아 Point로 변환하는 static 메서드 구현

--- BE/src/main/java/com/airbnb/utils/GeometryUtils.java
+++ BE/src/main/java/com/airbnb/utils/GeometryUtils.java
@@ -1,6 +1,9 @@
 package com.airbnb.utils;
 
 import com.airbnb.domain.Location;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 
 public class GeometryUtils {
 
@@ -35,5 +38,15 @@ public class GeometryUtils {
 
     private static Double cos(Double coordinate) {
         return Math.cos(coordinate);
+    }
+
+    public static Point toPoint(Double latitude, Double longitude) {
+        String pointWKT = String.format("POINT(%s %s)", longitude, latitude);
+        try {
+            return (Point) new WKTReader().read(pointWKT);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
