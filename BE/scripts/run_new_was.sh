713d697f4c6f13bee628c10287ef5e2e2e80fedd
jiwoo <zbqmgldjfh@gmail.com>
2022-05-30 20:54:14 +0900

    [fix]: run_new_was.sh 에서 인자 순서의 변경

--- BE/scripts/run_new_was.sh
+++ BE/scripts/run_new_was.sh
@@ -23,7 +23,7 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} --username=${USER_NAME} --password=${PASSWORD} /home/ec2-user/airbnb-deploy/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/airbnb-deploy/build/libs/* --username=${USER_NAME} --password=${PASSWORD} > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
 
