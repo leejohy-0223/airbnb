44c7ae66a73a813ffea4659feaa7fceb5f9c8eb6
jiwoo <zbqmgldjfh@gmail.com>
2022-05-30 20:37:40 +0900

    [style]: Health Class 이름 변경

--- BE/src/main/java/com/airbnb/api/HealthController.java
+++ BE/src/main/java/com/airbnb/api/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
-public class Health {
+public class HealthController {
 
     @GetMapping("/health")
     public String healtCheck() {
