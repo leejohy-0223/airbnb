78ee2899d6d91c8715540e2bb6142835829948b7
jiwoo <zbqmgldjfh@gmail.com>
2022-06-04 13:42:29 +0900

    [refactor]: User와 House의 관계 이름 수정
    - User와 House의 관계는 Wish 이지, WishList 관계가 아니다.

--- BE/src/main/java/com/airbnb/domain/Wish.java
+++ BE/src/main/java/com/airbnb/domain/Wish.java
@@ -3,7 +3,7 @@ package com.airbnb.domain;
 import javax.persistence.*;
 
 @Entity
-public class WishList {
+public class Wish {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +17,23 @@ public class WishList {
     @JoinColumn(name = "user_id")
     private User user;
 
+    public Wish() {
+    }
+
+    public Wish(House house, User user) {
+        this.house = house;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
