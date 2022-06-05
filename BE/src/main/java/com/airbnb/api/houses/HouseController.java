7ed81a3a02549a1d6db7325b94175f139945e83b
leejohy-0223 <leejohy@naver.com>
2022-06-05 10:12:13 +0900

    [refactor]: HouseController로 예약 API 이동

--- BE/src/main/java/com/airbnb/api/houses/HouseController.java
+++ BE/src/main/java/com/airbnb/api/houses/HouseController.java
@@ -1,5 +1,6 @@
 package com.airbnb.api.houses;
 
+import com.airbnb.api.houses.dto.AccommodationCostResponse;
 import com.airbnb.api.houses.dto.HouseDetailResponse;
 import com.airbnb.api.houses.dto.LocationInformationRequest;
 import com.airbnb.api.houses.dto.NumberOfHousesByPriceResponse;
@@ -7,8 +8,10 @@ import com.airbnb.api.houses.dto.SearchConditionRequest;
 import com.airbnb.service.HouseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -37,5 +40,11 @@ public class HouseController {
     @GetMapping("/price")
     public NumberOfHousesByPriceResponse findHouseCount(@ModelAttribute LocationInformationRequest request) {
         return houseService.findHouseCountInLocation(request);
+    }
+
+    @GetMapping("/{id}/reservation")
+    public AccommodationCostResponse calculateFee(@PathVariable Long id, @RequestParam @DateTimeFormat(pattern = "yyyyMMddHHmm") LocalDateTime startDateTime, @RequestParam @DateTimeFormat(pattern = "yyyyMMddHHmm") LocalDateTime endDateTime) {
+        log.info("[time] {}, {}", startDateTime, endDateTime);
+        return houseService.calculateFee(id, startDateTime, endDateTime);
     }
 }
