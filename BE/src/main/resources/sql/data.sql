0a9a8e1e40e2de868e25c9a463804924e2e82aba
jiwoo <zbqmgldjfh@gmail.com>
2022-06-06 10:15:00 +0900

    [feat]: Test용 data.sql에 wish 부분 추가

--- BE/src/main/resources/sql/data.sql
+++ BE/src/main/resources/sql/data.sql
@@ -10,3 +10,7 @@ VALUES (3, 4, 4.5, '아늑한 집', '원룸1', 'house1', ST_GeomFromText('POINT(
        (3, 4, 4.5, '아늑한 집', '원룸3', 'house3', ST_GeomFromText('POINT(127.029371 37.495423)'), 75000, 2),
        (3, 4, 4.5, '아늑한 집', '원룸4', 'house4', ST_GeomFromText('POINT(127.029371 37.495424)'), 75000, 2),
        (3, 4, 4.5, '아늑한 집', '원룸5', 'house5', ST_GeomFromText('POINT(127.029371 37.495425)'), 75000, 3);
+
+insert into wish (house_id, user_id)
+VALUES (2, 4),
+       (3, 4);
