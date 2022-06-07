038c8b4aea12bbc2b875a1719d5be93a5b0a05eb
jiwoo <zbqmgldjfh@gmail.com>
2022-06-07 13:45:03 +0900

    [feat]: run_new_was.sh 에 Oauth 정보 추가

--- BE/scripts/run_new_was.sh
+++ BE/scripts/run_new_was.sh
@@ -6,6 +6,9 @@ TARGET_PORT=0
 USER_NAME=$(env | grep username | cut -c 10-14)
 PASSWORD=$(env | grep password | cut -c 10-17)
 DATASOURCE=$(env | grep datasource | cut -c 12-97)
+GITHUB_CLIENT_SECRET=$(env | grep GITHUB_CLIENT_SECRET | cut -c 22-61)
+GITHUB_CLIENT_ID=$(env | grep GITHUB_CLIENT_ID | cut -c 18-37)
+KAKAO_CLIENT_ID=$(env | grep KAKAO_CLIENT_ID | cut -c 17-48)
 
 echo "> Current port of running WAS is ${CURRENT_PORT}."
 
@@ -24,7 +27,7 @@ if [ ! -z ${TARGET_PID} ]; then
   sudo kill ${TARGET_PID}
 fi
 
-nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/airbnb-deploy/build/libs/* --username=${USER_NAME} --password=${PASSWORD} --datasource=${DATASOURCE} > /home/ec2-user/nohup.out 2>&1 &
+nohup java -jar -Dserver.port=${TARGET_PORT} /home/ec2-user/airbnb-deploy/build/libs/* --username=${USER_NAME} --password=${PASSWORD} --datasource=${DATASOURCE} --KAKAO_CLIENT_ID=${KAKAO_CLIENT_ID} --GITHUB_CLIENT_ID=${GITHUB_CLIENT_ID} --GITHUB_CLIENT_SECRET=${GITHUB_CLIENT_SECRET} > /home/ec2-user/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
 exit 0
 
